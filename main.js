cc60132fb8e379996c1acd01bf6db99d76d12fec
AShershun <shershunchik17@gmail.com>
2024-01-16 20:28:51 +0200

    HW-3

---
+++ main.js
@@ -0,0 +1,4 @@
+let x = parseInt(prompt('Enter first number'));
+let y = parseInt(prompt('Enter second number'));
+let z = parseInt(prompt('Enter third number'));
+alert(`Your arithmetic average ${(x + y + z)/3}`);
