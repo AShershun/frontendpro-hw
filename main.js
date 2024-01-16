5dda718041162d2f200b8c578aba5c359a0440cf
AShershun <shershunchik17@gmail.com>
2024-01-16 20:58:52 +0200

    HW-3

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-let x = parseInt(prompt('Enter first number'));
-let y = parseInt(prompt('Enter second number'));
-let z = parseInt(prompt('Enter third number'));
-alert(`Your arithmetic average ${(x + y + z)/3}`);
+let x = +(prompt('Enter first number'));
+let y = +(prompt('Enter second number'));
+let z = +(prompt('Enter third number'));
+
+if(isNaN((x + y + z)/3)) {
+    alert(`You entered text, not a number`);
+
+} else {
+    alert(`Your arithmetic average ${(x + y + z)/3}`)
+}
+
