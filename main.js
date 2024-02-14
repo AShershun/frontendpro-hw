c2cbb0802b40aff4a1d8bb4da316f1d04eab8f95
AShershun <shershunchik17@gmail.com>
2024-02-14 21:04:23 +0200

    HW-19

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log('Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація)');
+let concatenateString = '';
+for(let i = 0; i<3; i++) {
+    let string = +prompt('Введіть текст:');
+    concatenateString += string + ' ';
+}
+alert(concatenateString.trim());
+
+console.log('Розбити за цифрами п\'ятизначне число і вивести у вихідному порядку через пробіл.');
+
+let number =  (+prompt("Введіть 5-значне число:")).toString();
+let stringNumber = '';
+
+for(let i = 0; i < number.length; i++) {
+    stringNumber += number.charAt(i) + ' ';
+}
+
+alert(stringNumber.trim());
