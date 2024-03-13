0614039dbb45d9026007ef6c46ddbaf209bb6ee0
AShershun <shershunchik17@gmail.com>
2024-03-14 00:48:46 +0200

    HW-26

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
+'use strict'
+
 const url = 'https://jsonplaceholder.typicode.com';
 const postsListElem = document.querySelector('#postList');
 const postInfoElem = document.querySelector('#postInfo');
-const postsTitle = document.querySelector('#post-title');
-const postsBody = document.querySelector('#post-body');
-const createBtn = document.querySelector('#create-post');
+const searchInput = document.querySelector('#search_input');
+const searchBtn = document.querySelector('#search_btn');
 
-let postsData = [];
+let postsData, commentsData = [];
 
-function createNewPost() {
-    postsListElem.innerHTML = '<div>Loading....</div>';
-
-    const newPost = {
-        userId: 1,
-        title: postsTitle.value,
-        body: postsBody.value
-    }
-
-    fetch(url + '/posts', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify(newPost),
-    }).then(() => {
-        postsData.push(newPost);
-        showAllPost(postsData);
-    });
-}
 
 function getPostComments(postId) {
     fetch(url + `/posts/${postId}/comments`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            commentsData = data;
+
+            showAllComments(commentsData)
         })
 }
 
@@ -40,18 +23,19 @@ function getPostComments(postId) {
 function showPostInfo(post) {
     postInfoElem.innerHTML = `
         <div class="postInfo">
-            <div class="post-title">${post.title}</div>
-            <div class="post-body">${post.body}</div>
-            <button onclick="getPostComments(${post.id})">Comments</button>
+            <div class="post_title">${capitalizeFirstLetter(post.title)}</div>
+            <div class="post_body">${capitalizeFirstLetter(post.body)}</div>
+            <button id="comments_btn" class="btn" onclick="getPostComments(${post.id})">Comments</button>
         </div>`;
 }
+
 
 function showAllPost(postsArr) {
     postsListElem.innerHTML = '';
 
     postsArr.forEach(post => {
         const tempPost = document.createElement('div');
-        tempPost.innerText = post.title;
+        tempPost.innerText = capitalizeFirstLetter(post.title);
         tempPost.classList.add('post');
 
         tempPost.addEventListener('click', () => {
@@ -61,6 +45,7 @@ function showAllPost(postsArr) {
         postsListElem.appendChild(tempPost);
     })
 }
+
 
 function getAllPosts() {
     fetch(url + '/posts')
@@ -72,6 +57,72 @@ function getAllPosts() {
 }
 
 
+//________________Новий функціонал________________
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+function showAllComments(commentsData) {
+    const commentBtn = document.querySelector('#comments_btn');
+    commentBtn.style.visibility = 'hidden';
+
+    const commentLabel = document.createElement('div');
+    commentLabel.textContent = '--Comments--';
+    commentLabel.classList.add('comment_label');
+    postInfoElem.appendChild(commentLabel);
+
+    commentsData.forEach(comment => {
+        const tempComment = document.createElement('div');
+        tempComment.innerHTML = `
+        <div class="comment_item">
+            <div class="comment_owner">${capitalizeFirstLetter(comment.email)} 
+            | ${capitalizeFirstLetter(comment.name)}</div>
+            <div class="comment_body">${capitalizeFirstLetter(comment.body)}</div>
+        </div>`;
+        tempComment.classList.add('comment');
+
+        postInfoElem.appendChild(tempComment);
+    });
+}
+
+function fetchPost(postId) {
+    return new Promise((resolve, reject) => {
+        fetch(url + `/posts/${postId}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Response was`t ok');
+                }
+                return res.json();
+            })
+            .then(post => {
+                resolve(post);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+
+searchBtn.addEventListener('click', () => {
+    const postId = searchInput.value;
+    // Напевно ця перевірка непротрібна раз в наc є Promise
+
+    // if (postId < 1 || postId > 100) {
+    //     alert('Введіть ID від 1 до 100');
+    //     searchInput.value = '';
+    //     return;
+    // }
+    fetchPost(postId)
+        .then(data => {
+            showPostInfo(data);
+        })
+        .catch(error => {
+            alert(error)
+        });
+    searchInput.value = '';
+})
+
 //Main
 getAllPosts();
-createBtn.addEventListener('click', () => createNewPost())
